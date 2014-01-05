0b3eb4b2a095335b7b938b4cb7e7a97f6307c700
JSantosP <javiersantospaniego@gmail.com>
2014-01-05 02:33:58 +0100

    Added .gitignore to avoid uploading target classes

---
+++ src/test/scala/org/jsantosp/euler/test/problem001.scala
@@ -0,0 +1,16 @@
+package org.jsantosp.euler.test
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSpec
+import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class Problem001 extends FunSpec with ShouldMatchers {
+	describe(""){
+		it(""){
+
+		}
+	}
+}
