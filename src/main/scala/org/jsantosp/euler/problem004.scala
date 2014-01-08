6b5a53db574c3c0f6f31bd696bbc6d91db9bc7fa
JSantosP <javiersantospaniego@gmail.com>
2014-01-08 01:59:44 +0100

    Solved Problem004

---
+++ src/main/scala/org/jsantosp/euler/problem004.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Javier Santos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jsantosp.euler
+
+/*
+ * Largest palindrome product
+ * Problem 4
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * 
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+object Problem004{
+
+	def solve: Int = {
+		(for {
+			a <- 100 until 1000
+			b <- 100 until 1000
+			val product = a * b
+			if s"$product" == s"$product".reverse
+		} yield product).sorted.last
+	}
+
+}
